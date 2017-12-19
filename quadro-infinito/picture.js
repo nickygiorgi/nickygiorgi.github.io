28d5d49d6ac691a2a0c109206e9eec619bb57f95
nicole giorgi <nicolegiorgi@gmail.com>
2017-12-19 08:20:49 +0000

    drawing random lines on canvas

--- quadro-infinito/picture.js
+++ quadro-infinito/picture.js
@@ -1,10 +1,21 @@
 class Picture {
-  constructor(width, height) {
-    this.height = height;
-    this.width = width;
+  constructor(canvas) {
+    this.canvas = canvas;
+	var picture = this;
+	setInterval(function(){ picture.drawRandomLine(); }, 1000);
   }
   
   randomPoint() {
-	  return new Point(randomNumber(0, this.width), randomNumber(0, this.height));
+	return new Point(randomNumber(0, this.canvas.width), randomNumber(0, this.canvas.height));
+  }
+  
+  drawRandomLine() {
+	var randomStart = this.randomPoint();
+	var randomEnd = this.randomPoint();
+	var context = this.canvas.getContext("2d");
+	context.beginPath();
+	context.moveTo(randomStart.x, randomStart.y);
+	context.lineTo(randomEnd.x, randomEnd.y);
+	context.stroke();
   }
 }
